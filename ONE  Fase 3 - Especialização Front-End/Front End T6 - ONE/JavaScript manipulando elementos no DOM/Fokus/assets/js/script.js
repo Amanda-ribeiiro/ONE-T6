f9d376fb73e13c692e409ef34181e9a4c154a8a9
Amanda  Ribeiro  Cerqueira <amanda_ribeiro98@yahoo.com.br>
2024-04-24 21:24:11 -0300

    add project fokus

---
+++ ONE  Fase 3 - Especialização Front-End/Front End T6 - ONE/JavaScript manipulando elementos no DOM/Fokus/assets/js/script.js
@@ -0,0 +1,16 @@
+const html = document.querySelector('html');
+const btFoco = document.querySelector('.app__card-button--foco');
+const btCurto = document.querySelector('.app__card-button--curto');
+const btLongo = document.querySelector('.app__card-button--longo');
+
+btFoco.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco')
+});
+
+btCurto.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto')
+});
+
+btLongo.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+});

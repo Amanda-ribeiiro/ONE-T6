14fa645a2031e3d65691237ea91cb25735f560b0
Amanda  Ribeiro  Cerqueira <amanda_ribeiro98@yahoo.com.br>
2024-06-14 23:46:09 -0300

    add project monibank

---
+++ ONE  Fase 3 - Especialização Front-End/Front End T6 - ONE/JavaScript validando formulários/monibank-main/js/script.js
@@ -0,0 +1,17 @@
+import eUmCPF from "./valida-cpf.js";
+import eMaiorDeIdade from "./validaIdade.js";
+
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
+});
+
+function verificaCampo(campo) {
+    if(campo.name == "cpf" && campo.value.length >= 11){
+        eUmCPF(campo);
+    }
+    if(campo.name == "aniversario" && campo.value != ""){
+        eMaiorDeIdade(campo);
+    }
+}
